b6fc4db2fd27808a0a47ed9ea6d2396d3186dc94
Kirill Karpov <karpov@apomaya.com>
2021-10-18 21:04:47 +0300

    add web sockets, cors packages

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { config } from 'dotenv';
+import WS from 'express-ws';
+import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 
 config({ path: '../.env' });
 
 const app = express();
+const WSServer = WS(app);
+const aWss = WSServer.getWss();
 
 const HOST = process.env.SERVER_HOST;
 const PORT = process.env.SERVER_PORT;
